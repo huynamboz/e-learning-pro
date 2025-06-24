1e77f77ff36663c791184b96f285bdfd0a3280f8
huynam-dana <nam.trinh@danaexperts.io.vn>
2025-06-24 22:13:20 +0700

    chore(lint): reconfig eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,10 @@
-// eslint.config.mjs
-import antfu from "@antfu/eslint-config";
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
 
-export default antfu();
+export default antfu({
+  stylistic: true, // enable stylistic formatting rules
+  typescript: true,
+  vue: true,
+  jsonc: false,
+  yml: false,
+})
